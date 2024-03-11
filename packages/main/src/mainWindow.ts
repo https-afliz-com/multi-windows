1da811cdd004ae67f673240e800ff5a4c6ed29f8
Jason Walker <anhluuxuan@gmail.com>
2024-03-11 21:27:50 +0700

    update src

--- packages/main/src/mainWindow.ts
+++ packages/main/src/mainWindow.ts
@@ -9,8 +9,9 @@ async function createWindow() {
       nodeIntegration: false,
       contextIsolation: true,
       sandbox: false, // Sandbox disabled because the demo of preload script depend on the Node.js api
-      webviewTag: false, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
+      webviewTag: true, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
       preload: join(app.getAppPath(), 'packages/preload/dist/index.mjs'),
+      webSecurity: false,
     },
   });
 
--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 // import {Rnd} from 'react-rnd';
 import {Space, NoPanArea} from 'react-zoomable-ui';
 import BrowserItem from './components/BrowserItem';
+import axios from 'axios';
 
 export const App = () => {
   const [value, setValue] = React.useState('');
   const [browserList, setBrowserList] = React.useState<any>([]);
+  const divRef = useRef<HTMLDivElement>(null);
+  const [data, setData] = React.useState();
 
   const handleAdd = () => {
     setBrowserList([
@@ -24,13 +27,26 @@ export const App = () => {
     setValue('');
   };
 
+  useEffect(() => {
+    if (divRef.current) {
+      const fetchData = async () => {
+        const rest = await axios.get('https://vnexpress.net/');
+        console.log(rest);
+        setData(rest.data);
+      };
+      fetchData();
+    }
+  }, []);
+
   return (
     <>
       <div
-        style={{width: '1920px', height: '1080px', background: 'red', position: 'relative'}}
+        style={{width: '500px', height: '500px', position: 'relative'}}
         className="wrapper-content"
+        ref={divRef}
       >
-        <Space
+        {/* <div dangerouslySetInnerHTML={{__html: data}}></div> */}
+        {/* <Space
           style={{border: 'solid 1px black', background: 'grey'}}
           onCreate={viewPort => {
             // viewPort.setBounds({x: [0, 800], y: [0, 600]});
@@ -47,9 +63,22 @@ export const App = () => {
               return item.components;
             })}
           </NoPanArea>
-        </Space>
+        </Space> */}
       </div>
+      {/* <webview
+        id="foo"
+        src="https://www.bing.com"
+        style={{display: 'inline-flex', width: '500px', height: '500px'}}
+      ></webview>
+      <iframe
+        id="foo"
+        src="https://www.wiki.org"
+        style={{display: 'inline-flex', width: '500px', height: '500px'}}
+        sandbox="allow-same-origin"
+      ></iframe> */}
       <br />
+      <div></div>
+      {/* <div>aaaa</div> */}
       <div style={{border: '2px solid black', padding: '1rem'}}>
         <div>
           <label>Input URL: </label>
